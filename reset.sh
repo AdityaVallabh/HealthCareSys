2767b316e663a436aff7d7016be01851c74f6632
AdityaVallabh <adityavallabhganti@gmail.com>
2018-04-21 22:11:57 +0530

    Reset DB

--- reset.sh
+++ reset.sh
@@ -2,9 +2,9 @@
 find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 find . -path "*/migrations/*.pyc"  -delete
 rm db.sqlite3
-python3 manage.py makemigrations
-python3 manage.py makemigrations accounts
-python3 manage.py makemigrations healthcare
-python3 manage.py migrate
-echo "exec(open('./healthcare/data/insert_hospitals.py').read());exec(open('./healthcare/data/insert_doctors.py').read())" | python3 manage.py shell
+python manage.py makemigrations accounts
+python manage.py makemigrations healthcare
+python manage.py makemigrations
+python manage.py migrate
+echo "exec(open('./healthcare/data/insert_hospitals.py').read());exec(open('./healthcare/data/insert_doctors.py').read())" | python manage.py shell
 exit
